dc2eaa9cd24c9c7fd0988476c2e5a3a17dd89a91
gowsik071 <gowsiksomasundaram@gmail.com>
2026-09-15 15:24:03 +0530

    Add files via upload

---
+++ Python task 3.py
@@ -0,0 +1,123 @@
+#For loop-Task
+
+'''for i in range(1,11):
+    print(i)
+'''
+
+'''for i in range(1,21):
+    if i%2==0:
+        print(i)
+'''
+
+'''for i in range(1,21,1):
+    if i%2!=0:
+        print(i)
+'''
+
+'''for i in range(1,11):
+    print(i,"*5=",i*5)
+'''
+
+'''sum=0
+for i in range(1,11):
+    sum=sum+i
+print("Total=",sum)    
+'''
+
+'''for i in "PYTHON":
+    print(i)
+'''
+
+'''text="Programming"
+count=0
+for i in text:
+    if i in "AEIOUaeiou":
+        count+=1
+print(count)
+'''
+
+'''for i in range(10,0,-1):
+    print(i)
+'''
+
+'''num=5
+fact=1
+for i in range(1,num+1):
+    fact*=i
+print("Factorial=",fact)
+'''
+
+'''for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()    
+'''
+
+'''name=("Gowsik")
+count=0
+for i in name:
+    if i in "aeiouAEIOU":
+        count=count+1
+print(count)        
+'''
+
+#While loop-Task
+
+'''i=1
+while i <=10:
+    print(i)
+    i+=1
+'''
+
+'''i=10
+while i >=1:
+    print(i)
+    i-=1
+'''
+
+'''i=2
+while i <=20:
+    print(i)
+    i+=2
+'''
+
+'''i=1
+total=0
+while i <=10:
+    total=total+i
+    i+=1
+print('sum=',total)    
+'''
+
+'''i=1
+while i <=10:
+    print(i,"x7=",i*7)
+    i+=1
+'''
+
+'''i=1
+fact=1
+while i <=5:
+    fact=fact*i
+    i+=1
+print("Factorial=",fact)
+'''
+
+'''num=12345673456789633334556
+count=0
+while num>0:
+    count+=1
+    num=num//10
+print("Digits=",count)    
+
+'''
+
+'''num=12345
+reverse=0
+while num >0:
+    digit=num%10
+    reverse=reverse*10+digit
+    num=num//10
+print(reverse)
+'''    
+
