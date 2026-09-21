4088aa5d9b7895390eb09325c5f0348fdf45ecc5
gowsik071 <gowsiksomasundaram@gmail.com>
2026-09-21 23:16:34 +0530

    Add files via upload

---
+++ Python task 5.py
@@ -0,0 +1,333 @@
+# Strings
+
+#1. Print the first 3 characters of a string using slicing.
+
+'''a="Python"
+print(a[0:3])
+'''
+
+#2. Print the last 3 characters of a string.
+
+'''a="Python"
+print(a[3:6])
+'''
+
+#3. Print the string in reverse.
+
+'''a="Learn Python"
+print(a[::-1])
+'''
+
+#4. Print every second character of a string.
+
+'''a="Python"
+print(a[0:6:2])
+'''
+
+#5. Print characters from index 2 to 6.
+
+'''a='programer'
+print(a[2:6])
+'''
+
+#6. Print the string without its first and last character.
+
+'''a="AI is booming"
+print(a[1:-1])
+'''
+
+#7. Extract the middle characters of a string.
+'''a="Hello"
+print(a[1:4])
+'''
+
+#8. Reverse a string and check whether it is a palindrome.
+
+'''a="madam"
+rev=a[::-1]
+if rev==a:
+    print("Palindrome")
+else:
+    print("Not palindrome")
+'''
+
+#9. Print characters at odd indexes.
+
+'''a='prgrammer'
+print(a[1:10:2])
+'''
+
+#10. Print characters at even indexes.
+
+'''a="pythonfullstack"
+print(a[0:15:2])
+'''
+
+# Lists
+
+#11. Print the first 3 elements of a list.
+
+'''a=[1,2,3,4,5]
+print(a[0:3])
+'''
+
+#12. Print the last 3 elements of a list.
+
+'''a=["apple","orange","graphs","lemon","pine apple","guava"]
+print(a[3:6])
+'''
+
+#13. Print a list in reverse order.
+
+'''a=[1,2,3,4,5,6]
+print(a[::-1])
+'''
+
+#14. Print every second element of a list.
+'''a=["mnc","ai","claude","chatgbt","gemini","replit"]
+print(a[0:5:2])
+'''
+
+#15. Print elements from index 2 to 5.
+
+'''a=["mnc","ai","claude","chatgbt","gemini","replit"]
+print(a[2:5])
+'''
+#16. Print the list without its first and last element.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[1:-1])
+'''
+
+#17. Extract the middle elements of a list.
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[4:6])
+'''
+
+#18. Print elements at even indexes.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[1:110:2])
+'''
+
+
+#19. Print elements at odd indexes.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[0:10:2])
+'''
+
+#20. Create a new list containing the last 5 elements using slicing.
+
+'''a=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+b=a[-5:]
+print(b)
+'''
+
+#Tuples
+
+#21. Print the first 3 elements of a tuple.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[0:3])
+'''
+
+#22. Print the last 3 elements.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[-3:])
+'''
+
+#23. Reverse a tuple using slicing.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[::-1])
+'''
+
+#24. Print every second element.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[1:10:2])
+'''
+
+#25. Print elements from index 1 to 4.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[1:4])
+'''
+
+
+#26. Print the tuple without its first and last element.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[1:-1])
+
+'''
+
+
+#27. Print elements at even indexes.
+
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[1:10:2])
+'''
+
+
+#28. Print elements at odd indexes.
+
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[0:10:2])
+'''
+
+
+#29. Extract the middle elements.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+print(a[4:6])
+'''
+
+
+#30. Create a new tuple containing the last 2 elements.
+
+'''a=[1,2,3,4,5,6,7,8,9,10]
+b=a[-2:]
+print(b)
+'''
+
+
+
+
+# Mixed Challenge
+
+#31. Given:
+
+#Print:
+
+#First 6 characters
+
+#Last 5 characters
+
+#Reverse
+
+#Every second character
+
+'''text="PythonProgramming"
+print(text[0:5])
+print(text[-5:])
+print(text[::-1])
+print(text[1:17:2])
+'''
+
+
+#32. Given
+
+#Print:
+
+#First 3 elements
+
+#Last 3 elements
+
+#Reverse
+
+#Elements from index 2 to 5
+
+
+'''numbers=[10, 20, 30, 40, 50, 60, 70]
+print(numbers[0:3])
+print(numbers[-3:])
+print(numbers[::-1])
+print(numbers[2:5])
+'''
+
+
+#33. Given:
+
+#Print:
+
+#First 2 elements
+
+#Last 2 elements
+
+#Reverse
+
+#Elements from index 1 to 3
+
+
+t = ("Python", "Java", "C", "AWS", "CCNA")
+print(t[0:2])
+print(t[-2:])
+print(t[::-1])
+print(t[1:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
